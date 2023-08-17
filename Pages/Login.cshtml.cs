ce75cd66390a514520ac96eb76bf0485bf1d7116
Baturhan <baturxan-06.72@mail.ru>
2023-08-17 11:07:40 +0600

    Add Enter page

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,14 +12,28 @@ namespace AuthApp.Pages
     public class Login : PageModel
     {
         private readonly ILogger<Login> _logger;
+        private readonly MyDbContext _context;
 
-        public Login(ILogger<Login> logger)
+        public Login(ILogger<Login> logger, MyDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost(string loginName, string loginPassword)
+        {
+            var userobj = _context.Users.FirstOrDefault(x => x.UName == loginName && x.UPassword == loginPassword);
+            if(userobj == null)
+            {
+                // ViewData["Not Found User"] = "Not correct Name or Password";
+                return RedirectToPage();
+            }
+
+            return RedirectToPage("/MainPage");
         }
     }
 }
